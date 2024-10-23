8f81fb1967bd8846582ca8391a90e3dfd802d487
suraifokkusu <79616065+suraifokkusu@users.noreply.github.com>
2024-10-23 18:26:32 +1000

    feat: добавлены роли ansible, обновлён README, добавлена проверка приложения

--- app/index.php
+++ app/index.php
@@ -1,2 +1,20 @@
 <?php
-echo "Hello, World!";
+// Настройки подключения к базе данных
+$host = 'mysql'; // Имя сервиса MySQL
+$dbname = 'exampledb'; // Имя вашей базы данных
+$user = 'exampleuser'; // Имя пользователя базы данных
+$password = 'examplepass'; // Пароль пользователя базы данных
+
+// Создаем подключение
+$dsn = "mysql:host=$host;dbname=$dbname;charset=utf8";
+$options = [
+    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+];
+
+try {
+    $pdo = new PDO($dsn, $user, $password, $options);
+    echo "Подключение к базе данных успешно!";
+} catch (PDOException $e) {
+    echo "Ошибка подключения: " . $e->getMessage();
+}
